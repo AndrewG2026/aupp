04a22a9ab9324d62f8a61afa3d912ef6ef5e326e
nickpavey <116367594+nickpavey@users.noreply.github.com>
2024-10-30 09:41:23 -0400

    Update aupp.py (#7)

--- aupp.py
+++ aupp.py
@@ -1,6 +1,29 @@
 from itertools import permutations
 import re 
 
+def print_sniper():
+    # print(" ___________ ")
+    # print(" \033[07m  cupp.py! \033[27m                # \033[07mC\033[27mommon")
+    # print("      \                     # \033[07mU\033[27mser")
+    # print("       \   \033[1;31m,__,\033[1;m             # \033[07mP\033[27masswords")
+    # print(
+    #     "        \  \033[1;31m(\033[1;moo\033[1;31m)____\033[1;m         # \033[07mP\033[27mrofiler"
+    # )
+    # print("           \033[1;31m(__)    )\ \033[1;m  ")
+    # print(
+    #     "           \033[1;31m   ||--|| \033[1;m\033[05m*\033[25m\033[1;m      [ Muris Kurgas | j0rgan@remote-exploit.org ]"
+    # )
+    # print(28 * " " + "[ Mebus | https://github.com/Mebus/]\r\n")
+    print("          \033[1;31m_________                   _______\033[1;m" + 5 * " " + "# \033[07mC\033[27mommon")
+    print("\033[1;31m_-----____/   ========================|______|\033[1;m" + 4 * " " + "# \033[07mU\033[27mser")
+    print("\033[1;31m|           ______________/\033[1;m" + 23 * " " + "# \033[07mP\033[27masswords")
+    print("\033[1;31m|    ___--_/(_)       ^\033[1;m" + 27 * " " + "# \033[07mP\033[27mrofiler")
+    print("\033[1;31m|___ ---\033[1;m")
+    print("This branch was created by the students at Anderson University")
+    print(18 * " " + "[ Muris Kurgas | j0rgan@remote-exploit.org ]")
+    print(25 * " " + "[ Mebus | https://github.com/Mebus/ ]\r\n")
+
+
 def interactive():
     """Implementation of the -i switch. Interactively question the user and
     create a password dictionary file based on the answer."""
